89e575b133763214cba61df6703821f39a02a501
Andreza Campbell <campbell.andreza@gmail.com>
2021-02-21 10:36:10 -0500

    uploading productslist and capturing input from add products form

--- routes/products.js
+++ routes/products.js
@@ -4,11 +4,47 @@ const model = require("../model/products");
 
 const router = express.Router();
 
-router.get("/", function(req, res, next) {
+const products1 = [{
+    name: "geladeira",
+    price: 800,
+    type: "eletronic"
+}, {
+    name: "arroz",
+    price: 30,
+    type: "food"
+}];
+
+
+
+const types = [{
+        id: "eletronic",
+        label: "Eletronico"
+    },
+    {
+        id: "food",
+        label: "Alimenticio"
+    },
+    {
+        id: "drugs",
+        label: "Farmaceutico"
+    }
+
+];
+router.get("/", function(req, res) {
+
     res.render("products", {
-        title: "produtos",
-        products: model.getProducts()
+        name: "Pagina de produtos",
+        types: types
     });
+});
+
+router.post('/', function(req, res) {
+    console.log(req.body);
+    res.render('products', { name: "Cadastro de produtos", types: types });
+});
+
+router.get('/lista', function(req, res) {
+    res.render('productsList', { products: products1 });
 });
 
 module.exports = router;
