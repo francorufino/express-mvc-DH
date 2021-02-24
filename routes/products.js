2d742406e83ac3905ecdda6e4ab2b19bddc2da65
Andreza Campbell <campbell.andreza@gmail.com>
2021-02-23 21:51:02 -0500

    subindo o delete de item vindo de formulario

--- routes/products.js
+++ routes/products.js
@@ -4,11 +4,13 @@ const model = require("../model/products");
 
 const router = express.Router();
 
-const products1 = [{
+let products1 = [{
+    id: 1,
     name: "geladeira",
     price: 800,
     type: "eletronic"
 }, {
+    id: 2,
     name: "arroz",
     price: 30,
     type: "food"
@@ -32,6 +34,8 @@ const types = [{
 ];
 router.get("/", function(req, res) {
 
+    products.push(req.body);
+
     res.render("products", {
         name: "Pagina de produtos",
         types: types
@@ -39,11 +43,25 @@ router.get("/", function(req, res) {
 });
 
 router.post('/', function(req, res) {
+    const newProduct = req.body;
+    newProduct.id = parseInt(Math.random() * 10000);
+    products1.push(newProduct);
     console.log(req.body);
     res.render('products', { name: "Cadastro de produtos", types: types });
 });
 
 router.get('/lista', function(req, res) {
+    res.render('productsList', { products: products1 });
+});
+
+router.delete('/:id', function(req, res) {
+    console.log("chamando o method delete");
+    console.log("detandoo o id: " + req.params.id);
+
+    products1 = products1.filter(function(product) {
+        return product.id !== parseInt(req.params.id);
+    });
+
     res.render('productsList', { products: products1 });
 });
 
